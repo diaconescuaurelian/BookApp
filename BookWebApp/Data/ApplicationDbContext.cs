aed961a9cdd85f35764406b8fe09aedb4264aa0f
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-03-07 18:05:53 +0200

    Add project files.

---
+++ BookWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebApp.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
